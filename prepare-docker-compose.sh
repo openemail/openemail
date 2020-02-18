e4c2254c5ebb4305dd868a75f6bf23887811c56f
openemail.io <48167083+openemail@users.noreply.github.com>
2020-02-18 17:54:03 +0530

    Update prepare-docker-compose.sh

--- prepare-docker-compose.sh
+++ prepare-docker-compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+cp docker-compose.yml docker-compose.yml.orig
 sed -i 's#image: mailcow/unbound:.*#image: hub.cybergate.lk/openemail/unbound:OPENEMAIL_VERSION#' docker-compose.yml
 sed -i 's#image: mariadb:.*#image: hub.cybergate.lk/openemail/mariadb:OPENEMAIL_VERSION#' docker-compose.yml
 sed -i 's#image: redis:.*#image: hub.cybergate.lk/openemail/redis:OPENEMAIL_VERSION#' docker-compose.yml
