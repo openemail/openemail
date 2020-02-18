5dc54cd1d07eac33add032ce4a8acd6d6f369e6f
openemail.io <48167083+openemail@users.noreply.github.com>
2020-02-18 16:28:46 +0530

    Update prepare-docker-compose.sh

--- prepare-docker-compose.sh
+++ prepare-docker-compose.sh
@@ -1,2 +1,16 @@
 sed -i 's#image: mailcow/unbound:.*#image: hub.cybergate.lk/openemail/unbound:OPENEMAIL_VERSION#' docker-compose.yml
 sed -i 's#image: mailcow/mariadb:.*#image: hub.cybergate.lk/openemail/mariadb:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/redis:.*#image: hub.cybergate.lk/openemail/redis:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/clamd:.*#image: hub.cybergate.lk/openemail/clamd:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/rspamd:.*#image: hub.cybergate.lk/openemail/rspamd:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/phpfpm:.*#image: hub.cybergate.lk/openemail/phpfpm:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/sogo:.*#image: hub.cybergate.lk/openemail/sogo:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/dovecot:.*#image: hub.cybergate.lk/openemail/dovecot:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/postfix:.*#image: hub.cybergate.lk/openemail/postfix:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/memcached:.*#image: hub.cybergate.lk/openemail/memcached:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/nginx:.*#image: hub.cybergate.lk/openemail/nginx:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/acme:.*#image: hub.cybergate.lk/openemail/acme:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/netfilter:.*#image: hub.cybergate.lk/openemail/netfilter:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/watchdog:.*#image: hub.cybergate.lk/openemail/watchdog:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/dockerapi:.*#image: hub.cybergate.lk/openemail/dockerapi:OPENEMAIL_VERSION#' docker-compose.yml
+sed -i 's#image: mailcow/solr:.*#image: hub.cybergate.lk/openemail/solr:OPENEMAIL_VERSION#' docker-compose.yml
