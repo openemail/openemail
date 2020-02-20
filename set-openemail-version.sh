9000571f575d0976d5d5f495918fd589ecae2947
openemail.io <48167083+openemail@users.noreply.github.com>
2020-02-20 11:17:04 +0530

    Update set-openemail-version.sh

--- set-openemail-version.sh
+++ set-openemail-version.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
-VERSION=$1
+echo "Enter Openemail version that you wish to deploy"
+while [ -z "${VERSION}" ]; do
+  read -p "Openemail version: " -e VERSION
+  DOTS=${VERSION//[^.]};
+  if [ ${#DOTS} -lt 2 ] && [ ! -z ${VERSION} ]; then
+    echo "${VERSION} is not in correct format"
+    VERSION=
+  fi
+done
+
 sed -i "s/OPENEMAIL_VERSION/${VERSION}/g" docker-compose.override.yml
