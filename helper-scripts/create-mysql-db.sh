e83a83a36f6e3ad81e7795540748dd07f96a0023
openemail.io <48167083+openemail@users.noreply.github.com>
2020-02-20 11:54:38 +0530

    Update create-mysql-db.sh

--- helper-scripts/create-mysql-db.sh
+++ helper-scripts/create-mysql-db.sh
@@ -3,6 +3,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR}/../
 source mailcow.conf
-docker exec -it $(docker ps -f name=mysql-mailcow -q) mysql -uroot -p${DBROOT} -e \
-    "$(docker exec -it $(docker ps -f name=mysql-mailcow -q) mysql -uroot -p${DBROOT} -e "SHOW DATABASES;")"
-  
+echo "Enter database name that you wish to create"
+read -p "Database name: " -e NEWDB
+docker exec -it $(docker ps -f name=mysql-mailcow -q) mysql -uroot -p${DBROOT} -e "CREATE DATABASE '${NEWDB}';GRANT ALL PRIVILEGES ON \`${NEWDB}\`.*
+TO '${DBUSER}'@'%';FLUSH PRIVILEGES;"
+
